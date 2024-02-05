f4b35ebc268f5a16c44911deaa7e4cfb5587adec
Mohsen <m.mehabadi@gmail.com>
2024-02-05 12:42:53 +0100

    add authentication by using clerk

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main>
-      <p>Home</p>
+      <UserButton afterSignOutUrl="/" />
     </main>
   );
 }
